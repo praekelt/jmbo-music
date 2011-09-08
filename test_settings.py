9da7b55073901398bd0323e16fe72f021942f309
Shaun Sephton <shaunsephton@gmail.com>
2011-09-08 16:23:51 +0200

    first commit

---
+++ test_settings.py
@@ -0,0 +1,16 @@
+DATABASE_ENGINE = 'sqlite3'
+
+INSTALLED_APPS = [
+    'django.contrib.contenttypes',
+    'django.contrib.sites',
+    'category',
+    'jmbo',
+    'music',
+    'photologue',
+    'publisher',
+    'secretballot',
+]
+
+CKEDITOR_MEDIA_PREFIX = '/media/ckeditor'
+
+SITE_ID = 1
