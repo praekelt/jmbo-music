4d6cf4d17476178dde1e0716289be87e0dbfa027
hedleyroos <hedleyroos@gmail.com>
2013-08-06 14:48:10 +0200

    Remove secret

--- test_settings.py
+++ test_settings.py
@@ -14,5 +14,5 @@ DATABASES = {
 
 JMBO_MUSIC = {
     'lastfm_api_key': 'c91d15b736995c4dac4e8b2b8cdbdb98 ',
-    'lastfm_api_secret': '3aa997f9cf9e655f44a35c79fb67544f',
+    'lastfm_api_secret': '',
 }
