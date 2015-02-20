2565724364eac8a548be2f59173e2f0630fa2f5d
hedleyroos <hedleyroos@gmail.com>
2015-02-20 15:42:54 +0200

    Include contributor in track feed

--- music/api.py
+++ music/api.py
@@ -2,12 +2,26 @@ from django.conf.urls.defaults import url
 
 from tastypie.resources import ModelResource
 from tastypie.constants import ALL
+from tastypie import fields
 from jmbo.api import ModelBaseResource
 
-from music.models import Track
+from music.models import Track, TrackContributor
+
+
+class TrackContributorResource(ModelBaseResource):
+
+    class Meta:
+        queryset = TrackContributor.permitted.all()
+        resource_name = 'trackcontributor'
+
+    def override_urls(self):
+        return [
+            url(r"^(?P<resource_name>%s)/(?P<slug>[\w-]+)/$" % self._meta.resource_name, self.wrap_view('dispatch_detail'), name="api_dispatch_detail"),
+        ]
 
 
 class TrackResource(ModelBaseResource):
+    contributor = fields.ToManyField(TrackContributorResource, 'contributor', full=True)
 
     class Meta:
         queryset = Track.permitted.all()
